2bab4bb77aa2968cc72676ee3011200ae161a4d6
SantiagoLira <120036630@upq.edu.mx>
2022-11-18 18:20:46 -0600

    creacion del navbar para el template

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -10,9 +11,53 @@
     <link rel="stylesheet" type="text/css" href="css/template.css" />
     <title>ADRE</title>
 </head>
+
 <body>
-<header>
-  <!-- Navbar -->
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light mask-custom shadow-0">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    <img src="img/logo.png" alt="Logo" width="100" height="100" class="d-inline-block align-text-top">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Almacen</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Reportes De Calidad</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Reportes De Recursos</a>
+                        </li>
+
+                    </ul>
+                    <ul class="navbar-nav d-flex flex-row">
+                        <li class="nav-item me-3 me-lg-0">
+                            <a class="nav-link" href="#!">
+                                <i class="fas fa-shopping-cart"></i>
+                            </a>
+                        </li>
+                        <li class="nav-item me-3 me-lg-0">
+                            <a class="nav-link" href="#!">
+                                <i class="fab fa-twitter"></i>
+                            </a>
+                        </li>
+                        <li class="nav-item me-3 me-lg-0">
+                            <a class="nav-link" href="#!">
+                                <i class="fab fa-instagram"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+       
+    </header>
+    <!-- <header>
   <nav class="navbar navbar-expand-lg navbar-light fixed-top mask-custom shadow-0">
     <div class="container">
     <a class="navbar-brand" href="#">
@@ -26,26 +71,15 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto">
           <li class="nav-item">
-            <a class="nav-link" href="#!">Offer</a>
+            <a class="nav-link" href="#!">Almacen</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#!">Features</a>
+            <a class="nav-link" href="#!">Reportes De Calidad</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#!">Portfolio</a>
+            <a class="nav-link" href="#!">Reportes de Recursos</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#!">Reference</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#!">About</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#!">Team</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#!">Contact</a>
-          </li>
+         
         </ul>
         <ul class="navbar-nav d-flex flex-row">
           <li class="nav-item me-3 me-lg-0">
@@ -67,21 +101,19 @@
       </div>
     </div>
   </nav>
-  <!-- Navbar -->
   <section>
-    <!-- Intro -->
     <div id="intro" class="bg-image vh-100" style="
           background-image: url('img/fondoLogo.png');
         ">
       <div class="mask" style="background-color: rgba(250, 182, 162, 0.15);"></div>
     </div>
-    <!-- Intro -->
   </section>
-</header>
-    
+</header>  -->
+
     @yield('contenido')
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-  
+
 </body>
+
 </html>
